185278486bf87ecb6422c38f50b60f80a5635509
wasifali591 <wasifali591@gmail.com>
2024-04-30 12:14:56 +0530

    controller to send message

---
+++ src/main/java/com/example/springbootrabbitmq/controllers/MessageController.java
@@ -0,0 +1,24 @@
+package com.example.springbootrabbitmq.controllers;
+
+import com.example.springbootrabbitmq.publishers.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+    private final RabbitMQProducer rabbitMQProducer;
+
+    public MessageController(RabbitMQProducer rabbitMQProducer) {
+        this.rabbitMQProducer = rabbitMQProducer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+        rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbitMQ ...");
+    }
+}
