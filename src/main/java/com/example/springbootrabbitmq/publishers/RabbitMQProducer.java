454b88296a16f4dbe21b0be5689758fa029a8738
wasifali591 <wasifali591@gmail.com>
2024-04-30 12:04:56 +0530

    implemented rabbitMq producer

---
+++ src/main/java/com/example/springbootrabbitmq/publishers/RabbitMQProducer.java
@@ -0,0 +1,28 @@
+package com.example.springbootrabbitmq.publishers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key.name}")
+    private String routingKey;
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    //send a message to exchange using routing key
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent: " + message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
